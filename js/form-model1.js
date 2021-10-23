7b42ac2f15183aaadadb0c7bcbcb071f1f8cf331
Lucas Bretas <66535036+lucasfront@users.noreply.github.com>
2021-10-23 10:33:48 -0300

    Add files via upload

---
+++ js/form-model1.js
@@ -0,0 +1,21 @@
+
+window.onload = function CreateForm(){
+
+        let formContainer = document.getElementById("form-container")
+
+        formContainer.innerHTML = `
+            <form id="form-model1" action="">
+                <h3 id="form-headline">Preencha o Formulário!</h3>
+                <p>Nome:</p>
+                <input type="email" name="" id="">
+                <p>E-mail:</p>
+                <input type="email" name="" id="">
+                <br>
+                <button>Fazer Cadastro</button>
+            </form>` 
+}
+
+
+
+
+            
